1b75c6d7f424e394401205dedd6ba6dabebea974
Patrick Spears <paralleldynamic@users.noreply.github.com>
2022-12-02 14:13:43 -0500

    build: successful challenge

--- src/challenges/Day02.kt
+++ src/challenges/Day02.kt
@@ -3,19 +3,47 @@ package challenges
 import utils.readInput
 
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+    fun part1(games: List<String>): Int = games
+        .map {
+            round -> when(round) {
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+                "A X" -> 4
+                "A Y" -> 8
+                "A Z" -> 3
+                "B X" -> 1
+                "B Y" -> 5
+                "B Z" -> 9
+                "C X" -> 7
+                "C Y" -> 2
+                "C Z" -> 6
+                else -> 0
+            }
+        }.sum()
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    fun part2(games: List<String>): Int = games
+        .map {
+            round -> when(round) {
+                "A X" -> 3
+                "A Y" -> 4
+                "A Z" -> 8
+                "B X" -> 1
+                "B Y" -> 5
+                "B Z" -> 9
+                "C X" -> 2
+                "C Y" -> 6
+                "C Z" -> 7
+                else -> 0
+            }
+        }.sum()
 
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    val testGames = readInput("Day02_test")
+    val games = readInput("Day02")
+
+    // part 1
+    check(part1(testGames) == 15)
+    println(part1(games))
+
+    // part 2
+    check(part2(testGames) == 12)
+    println(part2(games))
 }
