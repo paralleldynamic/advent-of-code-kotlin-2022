666bf44317f839958581a98fcfbf9f5790b8683c
Patrick Spears <paralleldynamic@users.noreply.github.com>
2022-12-03 10:39:25 -0500

    build: adjust challenge template

--- src/Setup.kts
+++ src/Setup.kts
@@ -1,9 +1,9 @@
 import java.io.File
 
-val template = File("challenges/Day02.kt")
+val template = File("challenges/Day03.kt")
 
-for (i in 3..25) {
+for (i in 4..25) {
     val dayNumber: String = i.toString().padStart(2, '0')
     val destination = File("challenges/Day$dayNumber.kt")
-    template.copyTo(destination)
+    template.copyTo(destination, overwrite = true)
 }
