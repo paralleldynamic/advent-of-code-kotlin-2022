07f26a833f8e9f77799a29f7ed5fc92c819dfa7b
Patrick Spears <paralleldynamic@users.noreply.github.com>
2022-12-04 14:08:53 -0500

    build: passing

--- src/challenges/Day04.kt
+++ src/challenges/Day04.kt
@@ -4,24 +4,39 @@ import utils.readInput
 
 private const val LABEL = "Day04"
 
-fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+fun checkContains(sections: List<String>): Boolean {
+    val (first_lower, first_upper) = sections.first().split("-").map { bound -> bound.toInt() }
+    val (second_lower, second_upper) = sections.last().split("-").map { bound -> bound.toInt() }
+    return (first_lower <= second_lower && first_upper >= second_upper)
+            || (first_lower >= second_lower && first_upper <= second_upper)
+}
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+fun checkOverlap(sections: List<String>): Boolean {
+    val (first_lower, first_upper) = sections.first().split("-").map { bound -> bound.toInt() }
+    val (second_lower, second_upper) = sections.last().split("-").map { bound -> bound.toInt() }
+    return second_lower <= first_upper && first_lower <= second_upper
+}
+
+fun main() {
+    fun part1(sections: List<String>): Int = sections
+        .map { pair -> pair.split(",") }
+        .map { sections -> checkContains(sections) }
+        .count { it }
+
+    fun part2(sections: List<String>): Int = sections
+        .map { pair -> pair.split(",") }
+        .map { sections -> checkContains(sections) || checkOverlap(sections) }
+        .count { it }
 
     // Reading input
-    val testInput = readInput("${LABEL}_test")
-    val input = readInput(LABEL)
+    val testSections = readInput("${LABEL}_test")
+    val sections = readInput(LABEL)
 
     // Part 1
-    check(part1(testInput) == 1)
-    println(part1(input))
+    check(part1(testSections) == 2)
+    println(part1(sections))
 
     // Part 2
-    check(part2(testInput) == 1)
-    println(part2(input))
+    check(part2(testSections) == 4)
+    println(part2(sections))
 }
